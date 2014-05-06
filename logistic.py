bd853aa622e97a22119c55234dbba067258746e3
liangkun <liangkun@outlook.com>
2014-05-06 18:34:10 +0800

    finish logistic regression chapter 05

--- logistic.py
+++ logistic.py
@@ -3,6 +3,7 @@
 import numpy as np
 import numpy.ma as ma
 import matplotlib.pyplot as pp
+import random
 
 
 def sigmoid(z):
@@ -44,6 +45,16 @@ def load_data(fname):
     return np.array(xs), np.array(ys)
 
 
+def normalize(xs):
+    """ Normalize all the feature into (0, 1)
+    """
+
+    max_values = np.max(xs, axis=0)
+    min_values = np.min(xs, axis=0)
+
+    return (xs - min_values) / (max_values - min_values), max_values, min_values
+
+
 def plot_data(xs, ys, theta=None):
     """ Plot the data points.
     """
@@ -69,9 +80,10 @@ def plot_data(xs, ys, theta=None):
     pp.show()
 
 
-def gradient_ascend(xs, ys, alpha = 0.1, max_iter = 1000, epslon = 1E-4, theta = None, plot = False):
+def gradient_ascend(xs, ys, alpha=0.1, max_iter=5000, epslon=1E-4, theta=None, plot=False):
     """ Run gradient acend to get the best fitted theta.
     """
+
     m, n = xs.shape
     xs = np.hstack((np.ones((m, 1)), xs))
     if theta is None:
@@ -97,7 +109,90 @@ def gradient_ascend(xs, ys, alpha = 0.1, max_iter = 1000, epslon = 1E-4, theta =
     return theta
 
 
+def stochastic_gradient_ascend(xs, ys, alpha=0.1, max_iter=20, epslon=1E-4, theta=None, plot=False):
+    """ Run stocchastic gradient ascend.
+    """
+
+    m, n = xs.shape
+    xs = np.hstack((np.ones((m,1)), xs))
+    if theta is None:
+        theta = np.zeros((n + 1))
+    likelihoods = []
+
+    last_likelihood = 1
+    for i in range(max_iter):
+        idxes = list(range(m))
+        for j in range(m):
+            new_alpha = 4 / (i + j+ 1) + alpha
+            randidx = random.randint(0, len(idxes)-1)
+            sample_idx = idxes[randidx]
+            likelihood, gradient = log_likelihood(xs[sample_idx, :], ys[sample_idx], theta)
+            del idxes[randidx]
+
+            likelihoods.append(likelihood)
+            # this does not apply because of the `big' variance of likelihood
+            #if abs(likelihood - last_likelihood) < epslon:
+            #    break
+            #last_likelihood = likelihood
+
+            theta += new_alpha * gradient
+
+    if plot:
+        pp.plot(np.arange(len(likelihoods)), likelihoods)
+        pp.legend(['likelihood trend'])
+        pp.show()
+
+    return theta
+
+
+def classify(theta, input):
+    if theta.dot(input) >= 0:
+        return 1
+    else:
+        return 0
+
+
+def colic_test(stochastic=False):
+    train_xs, train_ys = load_data('data/Ch05/horseColicTraining.txt')
+    train_xs, max_values, min_values = normalize(train_xs)
+
+    if stochastic:
+        theta = stochastic_gradient_ascend(train_xs, train_ys)
+    else:
+        theta = gradient_ascend(train_xs, train_ys)
+
+    test_xs, test_ys = load_data('data/Ch05/horseColicTest.txt')
+    test_xs = (test_xs - min_values) / (max_values - min_values)
+    test_count = len(test_ys)
+    test_xs = np.hstack((np.ones((test_count, 1)), test_xs))
+    test_error = 0
+    for i in range(test_count):
+        label = classify(theta, test_xs[i, :])
+        if label != test_ys[i]:
+            test_error += 1
+
+    error_rate = test_error / test_count
+    print('Error rate on test set is %{}'.format(error_rate * 100))
+    return error_rate
+
+
+def multiple_test(n=10, stochasitic=False):
+    if not stochasitic:
+        colic_test(stochasitic)
+    else:
+        error_rates = 0.0
+        for i in range(n):
+            error_rates += colic_test(stochasitic)
+
+        print('\n{} times tests, average error rate is %{}'.format(n, error_rates / n * 100))
+
+
 if __name__ == '__main__':
-    xs, ys = load_data('data/Ch05/testSet.txt')
-    theta = gradient_ascend(xs, ys, plot=True)
-    plot_data(xs, ys, theta)
+    #xs, ys = load_data('data/Ch05/testSet.txt')
+    #xs, ys = load_data('data/Ch05/horseColicTraining.txt')
+    #theta = gradient_ascend(xs, ys, plot=True)
+    #plot_data(xs, ys, theta)
+    #theta = stochastic_gradient_ascend(xs, ys, plot=True)
+    #plot_data(xs, ys, theta)
+    multiple_test(stochasitic=True)
+    multiple_test()
