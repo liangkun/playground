72afe16cc74f1ae276e90d9016aa29223cec4a2e
Liang Kun <liangkun@ishumei.com>
2022-02-02 16:22:32 +0800

    update

--- bustruck.py
+++ bustruck.py
@@ -42,3 +42,30 @@ class BusTruckDataset(Dataset):
     
     def collate_fn(self, batch):
         pass
+
+def prepare_dataset(df_path, image_root, image_w, image_h, nsamples, name2label):
+    # Prepare BusTruckDataset
+    raw = pd.read_csv(df_path)
+    image_ids = raw['ImageID'].unique().tolist()[:nsamples]
+    image_labels = []
+    image_gtbbs = []
+
+    for image_id in image_ids:
+        imagedf = raw[raw['ImageID'] == image_id]
+        image_labels.append(
+            torch.tensor([name2label[name] for name in imagedf['LabelName'].values.tolist()])
+        )
+        boxes = imagedf[['XMin', 'YMin', 'XMax', 'YMax']].values
+        boxes[:, [0, 2]] *= image_w
+        boxes[:, [1, 3]] *= image_h
+        image_gtbbs.append(torch.Tensor(boxes).float())
+
+    samples = BusTruckDataset(image_root, image_ids, image_labels, image_gtbbs, h=image_h, w=image_w)
+    n_samples = len(samples)
+    n_train = int(n_samples * 0.7)
+    n_val = (n_samples - n_train) // 2
+    n_test = n_samples - n_train - n_val
+
+    train_ds, val_ds, test_ds = random_split(samples, [n_train, n_val, n_test])
+    print('trainset: %d, valset: %d, testset: %d' % (len(train_ds), len(val_ds), len(test_ds)))
+    return train_ds, val_ds, test_ds
--- object-main.py
+++ object-main.py
@@ -15,7 +15,7 @@ from torchsummary import summary
 import numpy as np
 
 from FRCNN import get_model
-from bustruck import BusTruckDataset
+from bustruck import BusTruckDataset, prepare_dataset
 
 # input params
 # IMAGE_ROOT = 'F:\\datasets\\bus-trucks\\images\\images'
@@ -31,33 +31,6 @@ N_SAMPLES = 500
 
 def collate_fn(batch):
     return tuple(zip(*batch))
-
-def prepare_dataset():
-    # Prepare BusTruckDataset
-    raw = pd.read_csv(DF_PATH)
-    image_ids = raw['ImageID'].unique().tolist()[:N_SAMPLES]
-    image_labels = []
-    image_gtbbs = []
-
-    for image_id in image_ids:
-        imagedf = raw[raw['ImageID'] == image_id]
-        image_labels.append(
-            torch.tensor([NAME2LABEL[name] for name in imagedf['LabelName'].values.tolist()])
-        )
-        boxes = imagedf[['XMin', 'YMin', 'XMax', 'YMax']].values
-        boxes[:, [0, 2]] *= IMAGE_W
-        boxes[:, [1, 3]] *= IMAGE_H
-        image_gtbbs.append(torch.Tensor(boxes).float())
-
-    samples = BusTruckDataset(IMAGE_ROOT, image_ids, image_labels, image_gtbbs, h=IMAGE_H, w=IMAGE_W)
-    n_samples = len(samples)
-    n_train = int(n_samples * 0.7)
-    n_val = (n_samples - n_train) // 2
-    n_test = n_samples - n_train - n_val
-
-    train_ds, val_ds, test_ds = random_split(samples, [n_train, n_val, n_test])
-    print('trainset: %d, valset: %d, testset: %d' % (len(train_ds), len(val_ds), len(test_ds)))
-    return train_ds, val_ds, test_ds
 
 def plot_samples(ds, nrow, ncol, figsize=(6, 6)):
     # plot a image to verify
@@ -95,7 +68,7 @@ def validate_batch(xs, ys, model):
 
 if __name__ == "__main__":
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    train_ds, val_ds, test_ds = prepare_dataset()
+    train_ds, val_ds, test_ds = prepare_dataset(DF_PATH, IMAGE_ROOT, IMAGE_W, IMAGE_H, N_SAMPLES, NAME2LABEL)
     train_dl = DataLoader(train_ds, batch_size=8, shuffle=True, drop_last=True, collate_fn=collate_fn)
     val_dl = DataLoader(val_ds, batch_size=1, shuffle=False, collate_fn=collate_fn)
     test_dl = DataLoader(test_ds, batch_size=1, shuffle=False, collate_fn=collate_fn)
